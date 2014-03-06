a02a2d7adb51843af028ba7b38f7f4496500aab1
hoyin29 <hoyin29@gmail.com>
2014-03-05 23:19:53 -0800

    renaming classes

---
+++ PayFive/src/team/cs6365/payfive/database/MenuItemDatabaseContract.java
@@ -0,0 +1,18 @@
+package team.cs6365.payfive.database;
+
+import android.provider.BaseColumns;
+
+public abstract class MenuItemDatabaseContract implements BaseColumns
+{
+    public static final String TABLE_NAME = "menuitem";
+    public static final String COLUMN_NAME_NAME = "name";
+    public static final String COLUMN_NAME_CATEGORY = "category";
+    public static final String COLUMN_NAME_DESCRIPTION = "description";
+    public static final String COLUMN_NAME_PRICE = "price";
+    public static final String COLUMN_NAME_THUMBNAIL = "thumbnail";
+    
+    private MenuItemDatabaseContract()
+    {
+    	
+    }
+}
