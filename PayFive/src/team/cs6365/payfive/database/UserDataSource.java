84486b94bd50b01dbce15402cbeb8243c2b7069a
hoyin29 <hoyin29@gmail.com>
2014-03-13 11:37:41 -0700

    user db helper, ds

--- PayFive/src/team/cs6365/payfive/database/UserDataSource.java
+++ PayFive/src/team/cs6365/payfive/database/UserDataSource.java
@@ -1,5 +1,99 @@
 package team.cs6365.payfive.database;
 
-public class UserDataSource {
+import java.util.ArrayList;
+import java.util.List;
 
+import team.cs6365.payfive.model.User;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
+
+public class UserDataSource {
+	
+	private SQLiteDatabase db;
+	private UserDatabaseHelper dbHelper;
+	private String[] columns = {UserDatabaseContract.COLUMN_NAME_NAME,
+			UserDatabaseContract.COLUMN_NAME_PAYPALID};
+	
+	public UserDataSource(Context context)
+	{
+		dbHelper = new UserDatabaseHelper(context);
+	}
+	
+	public void open() throws SQLException
+	{
+		db = dbHelper.getWritableDatabase();
+	}
+	
+	public void close() 
+	{
+		dbHelper.close();
+	}
+	
+	public void addUser(String name, String paypalId) 
+	{
+		ContentValues row = new ContentValues();
+		row.put(columns[0], name);
+		row.put(columns[1], paypalId);
+		db.insert(UserDatabaseContract.TABLE_NAME, null, row);
+	}
+	
+	public void deleteUser(User user)
+	{
+		db.delete(UserDatabaseContract.TABLE_NAME, 
+				UserDatabaseContract.COLUMN_NAME_NAME + "='" + user.getName() + "' AND " + 
+				UserDatabaseContract.COLUMN_NAME_PAYPALID + "='" + user.getPaypalId(), null);
+	}
+	
+	public User getUser(String name, String paypalId)
+	{
+		User u = new User();
+		
+		Cursor cur = db.query(UserDatabaseContract.TABLE_NAME, 
+				columns,
+				UserDatabaseContract.COLUMN_NAME_NAME + "='" + name + "' AND " + 
+				UserDatabaseContract.COLUMN_NAME_PAYPALID + "='" + paypalId + "'", 
+				null, null, null, null);
+		
+		if(cur != null && cur.getCount() > 0)
+		{
+			cur.moveToFirst();
+			u = cursorToUser(cur);	
+		}
+		
+		cur.close();
+		return u;
+	}
+	
+	public List<User> getAllUser()
+	{
+		List<User> us = new ArrayList<User>();
+		
+		Cursor cur = db.query(UserDatabaseContract.TABLE_NAME, 
+				columns, null, null, null, null, null);
+		
+		if(cur != null && cur.getCount() > 0)
+		{
+			cur.moveToFirst();
+			while(!cur.isAfterLast())
+			{
+				us.add(cursorToUser(cur));
+				cur.moveToNext();
+			}
+		}
+		
+		cur.close();
+		return us;
+	}
+	
+	public User cursorToUser(Cursor cur)
+	{
+		User u = new User(cur.getString(1),
+				cur.getString(2));
+		
+		return u;
+	}
 }
--- PayFive/src/team/cs6365/payfive/database/UserDatabaseHelper.java
+++ PayFive/src/team/cs6365/payfive/database/UserDatabaseHelper.java
@@ -5,20 +5,18 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class UserDatabaseHelper extends SQLiteOpenHelper {
+	
 	public static final int DATABASE_VERSION = 1;
 	public static final String DATABASE_NAME = "user.db";
 
 	public static final String SQL_CREATE_USER_TABLE = 
-			"CREATE TABLE " +  MenuItemDatabaseContract.TABLE_NAME + " (" + 
-					MenuItemDatabaseContract._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-					MenuItemDatabaseContract.COLUMN_NAME_NAME + " TEXT NOT NULL, " +  
-					MenuItemDatabaseContract.COLUMN_NAME_CATEGORY + " TEXT NOT NULL, " + 
-					MenuItemDatabaseContract.COLUMN_NAME_DESCRIPTION + " TEXT NOT NULL, " + 
-					MenuItemDatabaseContract.COLUMN_NAME_PRICE + " REAL NOT NULL, " +
-					MenuItemDatabaseContract.COLUMN_NAME_THUMBNAIL + " BLOB NOT NULL)";
+			"CREATE TABLE " +  UserDatabaseContract.TABLE_NAME + " (" + 
+					UserDatabaseContract._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+					UserDatabaseContract.COLUMN_NAME_NAME + " TEXT NOT NULL, " +  
+					UserDatabaseContract.COLUMN_NAME_PAYPALID + " TEXT NOT NULL)";
 
-	public static final String SQL_DROP_MENUITEM_TABLE = 
-			"DROP TABLE IF EXISTS " + MenuItemDatabaseContract.TABLE_NAME;
+	public static final String SQL_DROP_USER_TABLE = 
+			"DROP TABLE IF EXISTS " + UserDatabaseContract.TABLE_NAME;
 
 	public UserDatabaseHelper(Context context) 
 	{
@@ -27,12 +25,12 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
 
 	public void onCreate(SQLiteDatabase db) 
 	{
-		db.execSQL(SQL_CREATE_MENUITEM_TABLE);
+		db.execSQL(SQL_CREATE_USER_TABLE);
 	}
 
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) 
 	{
-		db.execSQL(SQL_DROP_MENUITEM_TABLE);
+		db.execSQL(SQL_DROP_USER_TABLE);
 		onCreate(db);
 	}
 
